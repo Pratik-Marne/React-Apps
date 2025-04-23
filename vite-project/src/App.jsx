2b304e9e7c362aa6bd5f194902fc0b2894979c07
pratik-marne <pratikmarne4142@gmail.com>
2025-04-23 05:23:50 -0700

    Added counter project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,46 @@
+
+
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [counter, setCounter] = useState(0);
+  const [isRunning,setIsRunning] = useState(false)
+  const timeRef = useRef(null);
+
+  useEffect(()=>{
+    if(isRunning){
+      timeRef.current = setInterval(() => {
+       setCounter((prev)=>{
+        return prev < 15 ? prev + 1 : 0 
+       }) 
+      }, 1000);
+    }
+    return () => clearInterval(timeRef.current)
+  },[isRunning])
+
+  const handleStart = () =>{
+    if(!isRunning){
+      setIsRunning(true)
+    }
+  }
+ 
+  const handleStop = () =>{
+    setIsRunning(false)
+    clearInterval(timeRef.current)
+  }
+
+  
+
+
+  return (
+    <>
+     <h1 className='text-5xl font-bold'>Counter:{counter} </h1>
+     <button onClick={handleStart}>Start</button>
+     <button onClick={handleStop}>Stop</button>
+    </>
+  )
+}
+
+export default App
